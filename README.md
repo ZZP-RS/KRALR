fc78b632b1d98f169386c81d7b6e37ac12cbc2d3
Zyh <zzp_1988@163.com>
2023-05-31 10:30:38 +0800

    Model upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # KRALR
+
+## Environment Requirement
+The code has been tested running under Python 3.7.10. The required packages are as follows:
+* torch == 1.8.0
+* numpy == 1.23.5
+* pandas == 1.5.3
+* scipy == 1.10.1
+* tqdm == 4.65.0
+* scikit-learn == 1.2.2
+
+## KRALR operation steps
+
+1. run Neighbors_Sample.py to generate Long-tail Neighbors
+2. run doCooccur.py to construct Co-occurrence graph and merge Co-occurrence graph and KG
+3. Run code 'python main_KRALR.py' to start KRALR
---
+++ doCooccur.py
@@ -0,0 +1,61 @@
+import collections
+
+cf_file = 'datasets/last-fm/train.txt'
+kg_file = 'datasets/last-fm/kg_final.txt'
+save_file = 'datasets/last-fm/kg_final2.txt'
+
+nodes = collections.defaultdict(list)
+items = set()
+# load cf_data
+lines = open(cf_file, 'r').readlines()
+for line in lines:
+    temp = [int(x) for x in line.split()]
+    items.add(temp[1])
+lines = open(cf_file, 'r').readlines()
+for line in lines:
+    temp = [int(x) for x in line.split()]
+    if temp[1] not in nodes.keys():
+        nodes[temp[1]] = [temp[0]]
+    else:
+        nodes[temp[1]].append(temp[0])
+# load kg_data
+h = []
+r = []
+t = []
+lines = open(kg_file, 'r').readlines()
+for line in lines:
+    temp = [int(x) for x in line.split()]
+    assert len(temp) == 3
+    h.append(temp[0])
+    r.append(temp[1])
+    t.append(temp[2])
+    if temp[0] in nodes.keys():
+        nodes[temp[0]].append(temp[2])
+    if temp[2] in nodes.keys():
+        nodes[temp[2]].append(temp[0])
+
+assert len(h) == len(r)
+assert len(h) == len(t)
+n_relations = len(set(r))
+n_h_origin = len(h)
+print(n_h_origin)
+print(n_relations)
+item_list = list(nodes.keys())
+intersection_num = 0
+for i in range(len(item_list)):
+    target_set = set(nodes[item_list[i]])
+    for j in range(i + 1, len(item_list)):
+        object_set = set(nodes[item_list[j]])
+        if len(target_set.intersection(object_set)) > 3:
+            h.append(item_list[i])
+            r.append(n_relations)
+            t.append(item_list[j])
+            intersection_num += 1
+print(intersection_num)  # 2299 15,518
+writer = open(save_file, 'w')
+assert len(h) == len(r)
+assert len(h) == len(t)
+assert len(h) == intersection_num + n_h_origin
+for i in range(len(h)):
+    writer.write('%d %d %d\n' % (h[i], r[i], t[i]))
+writer.close()
